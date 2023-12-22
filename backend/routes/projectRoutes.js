60922aa9b7923b9512b1b26cfcc71494654fb1fe
Petal Ladenson <habonimkef@gmail.com>
2023-12-21 18:34:02 -0700

    feat: route files set up

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const { projectCtrl } = require("../controllers")
+
+module.exports = router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const { userCtrl } = require("../controllers")
+
+module.exports = router;
