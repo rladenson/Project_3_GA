70345cd2dd9e5d764421733c6f1c49f4a23dfa1b
Petal Ladenson <habonimkef@gmail.com>
2023-12-23 13:44:43 -0700

    feat: get user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const { userCtrl } = require("../controllers");
 const { isAuthenticated } = require("../middleware/index");
 
 // TODO ROUTES GO HERE
+router.get("/:username", userCtrl.getUser);
 router.post("/", userCtrl.createUser);
 router.patch("/", isAuthenticated, userCtrl.updateUser);
 router.delete("/", isAuthenticated, userCtrl.deleteUser);
