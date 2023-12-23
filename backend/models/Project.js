ce73ad88ba984d3ff8bc091fc0fba0cdf2f27568
bRad1995 <142275327+bRad1995@users.noreply.github.com>
2023-12-23 14:18:13 -0600

    Project schema created. (#9)

---
+++ backend/models/Project.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    user: {type: mongoose.ObjectId, required: true},
+    img: {type: [String], required: false},
+    name: {type: String, required: true},
+    repolink: {type: String, required: true},
+    deployedlink: {type: String, required: true},
+    techused: {type: [String], required: true},
+    description: {type: String, required: false},
+    tags: {type: [String], required: true},
+    authorname: {type: String, required: true},
+
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+module.exports = Project
