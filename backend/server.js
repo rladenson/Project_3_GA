a3666774d33382d97dff26718eaeec394efe24af
Petal Ladenson <habonimkef@gmail.com>
2023-12-23 09:33:41 -0700

    fix: routing and nodemon now work

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
-const {PORT} = process.env;
-const express = require('express')
+const { PORT } = process.env;
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const routes = require("./routes/index");
 
-const connection = require('./models/index')
+const connection = require("./models/index");
 
-app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`))
+app.use("/", routes);
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
