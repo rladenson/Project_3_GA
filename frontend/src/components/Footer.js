e3acd73e4bc32e23c0af0e94650bd215e23b618c
karolbgm <141590325+karolbgm@users.noreply.github.com>
2024-01-01 18:17:16 -0700

    Frontend folder (#13)
    * feat: header footer main and profile page
    
    * feat: ShowProject page
    
    * feat: NewProfile page

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <>
+      <footer className="bg-dark py-3 mt-auto">
+        <div className="container">
+          <span className="text-white-50">&copy; devPort 2023</span>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
