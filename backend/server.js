fbc8b0aa3d7583f70f747dab8412104e7b88eeff
karolbgm <karolyne.morgan@outlook.com>
2024-01-05 21:18:52 -0700

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,35 @@
 require("dotenv").config();
-const { PORT } = process.env;
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 const app = express();
-const connection = require('./models/index')
+// const connection = require('./models/index')
 const cors = require("cors");
-const routes = require("./routes/index");
-const session = require("express-session");
+// const routes = require("./routes/index");
+const db = mongoose.connection; //default connection object
 
+// ENV VARIABLES
+const mongoURI = process.env.MONGOURI;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-    session({
-        secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
-        resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
-        saveUninitialized: false, // default  more info: https://www.npmjs.com/package/express-session#resave
-    })
-);
 
-app.use("/", routes);
+
+// app.use("/", routes);
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
+
+
+
+//CONNECT TO MONGO
+mongoose.connect(mongoURI);
+
+//ERROR/SUCCESS
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", mongoURI));
+db.on("disconnected", () => console.log("mongo disconnected"));
+db.on("open", () => {
+  console.log("Mongoose connection is open");
+});
