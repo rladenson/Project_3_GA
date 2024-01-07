2f9712be517e13f167670911b58b2f8bbb3eea8e
TraceyC <tacarpenter216@gmail.com>
2024-01-06 19:36:13 -0500

    home link (#29)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -45,7 +45,7 @@ function Header() {
     <>
       <Navbar expand="lg" bg="dark" data-bs-theme="dark">
         <Container fluid>
-          <Navbar.Brand href="#">devPort</Navbar.Brand>
+          <Navbar.Brand href="/Home">devPort</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
