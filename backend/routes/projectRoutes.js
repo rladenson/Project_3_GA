e0fb745a946cc1e81982a85d3488112ae8193180
karolbgm <karolyne.morgan@outlook.com>
2024-01-07 18:47:18 -0700

    feat ShowProject

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,11 +1,11 @@
 const projectRouter = require("express").Router();
-const { createProject, getAllProjects, deleteProject, explore } = require('../controllers/projectCtrls')
+const { createProject, getAllProjects, deleteProject } = require('../controllers/projectCtrls')
 const requireLogin = require('../middleware/requireLogin')
 
 projectRouter.post("/createproject",requireLogin, createProject)
 projectRouter.get("/allproject",requireLogin,getAllProjects)
 
 //delete post
-projectRouter.delete("/deleteproject/:projectId",requireLogin,deleteProject)
+// projectRouter.delete("/deleteproject/:projectId",requireLogin,deleteProject)
 
 module.exports = projectRouter
