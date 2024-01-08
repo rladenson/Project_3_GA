0eb2d664fbe3af5de507a7d9752b1ed812fbccc5
Petal Ladenson <habonimkef@gmail.com>
2024-01-07 18:35:54 -0700

    backend: delete project

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,11 +1,10 @@
 const projectRouter = require("express").Router();
 const { createProject, getAllProjects, deleteProject, explore } = require('../controllers/projectCtrls')
-const requireLogin = require('../middleware/requireLogin')
+const requireLogin = require('../middleware/requireLogin');
+const requireOwned = require("../middleware/requireOwned");
 
 projectRouter.post("/createproject",requireLogin, createProject)
-projectRouter.get("/allproject",requireLogin,getAllProjects)
-
-//delete post
-projectRouter.delete("/deleteproject/:projectId",requireLogin,deleteProject)
+projectRouter.get("/allproject", getAllProjects)
+projectRouter.delete("/deleteproject/:id", requireOwned, deleteProject)
 
 module.exports = projectRouter
